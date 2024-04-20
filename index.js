0d59f6059eef99c785cc3f1cea954a5b71ca7957
Cédric Lang-Roth <contact@clangroth.fr>
2024-04-20 12:49:06 +0200

    With the right variable names.

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const DAY_AMOUNT = 80;
 let updated = final;
 
 for (let i = 0; i < DAY_AMOUNT; i++) {
-  let newBird = [];
+  let newFish = [];
   updated = updated.map((item) => {
-    let birdAge = Number(item);
-    if (birdAge <= 8 && birdAge > 0) {
-      return birdAge - 1;
-    } else if (birdAge === 0) {
-      newBird.push(8);
+    let fishAge = Number(item);
+    if (fishAge <= 8 && fishAge > 0) {
+      return fishAge - 1;
+    } else if (fishAge === 0) {
+      newFish.push(8);
       return 6;
     }
   });
-  updated = [...updated, ...newBird];
+  updated = [...updated, ...newFish];
 }
 
 console.log(updated.length);
