5ae0daa77f7ea88291c5cf8bbddae80b84efbdc6
Cédric Lang-Roth <contact@clangroth.fr>
2024-04-20 18:56:43 +0200

    Final solution of mine

--- index.js
+++ index.js
@@ -23,4 +23,22 @@ const part1 = (data) => {
   return updated.length;
 };
 
+const part2 = (data) => {
+  const DAY_AMOUNT = 256;
+  const ages = data
+    .split(",")
+    .map(Number)
+    .reduce((acc, curr) => {
+      acc[curr] = acc[curr] + 1;
+      return acc;
+    }, new Array(9).fill(0));
+  for (let day = 0; day < DAY_AMOUNT; day++) {
+    const age0 = ages.shift();
+    ages.push(age0);
+    ages[6] += age0;
+  }
+  return ages.reduce((acc, curr) => acc + curr);
+};
+
 console.log(part1(final));
+console.log(part2(final));
